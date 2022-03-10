707c94254abfa53f2fbccb99e7ddde84e7c46e27
Franco Campaiola <francocampaiola@hotmail.com>
2022-03-10 20:37:20 -0300

    :+1: Se comienza con el proyecto.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F5104E 100%);
+  background-size: 300px;
+  font-family: Arial, Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  cursor: pointer;
+`;
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`
+
 
 function App() {
+  const [frase, guardarFrase] = useState({});
+  
+  const consultarAPI = async () => {
+  const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  const frase = await api.json();
+  guardarFrase(frase[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener frase
+      </Boton>
+    </Contenedor>
   );
 }
 
