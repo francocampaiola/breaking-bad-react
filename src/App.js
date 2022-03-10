478556454d7844809bf3fdbb3f888cfee3e3bf3d
Franco Campaiola <francocampaiola@hotmail.com>
2022-03-10 20:49:53 -0300

    :+1: Se incorpora la API a utilizar en el proyecto.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Frase from "./Components/Frase";
 import styled from "@emotion/styled";
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F5104E 100%);
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
   background-size: 300px;
   font-family: Arial, Arial, Helvetica, sans-serif;
   color: #fff;
@@ -10,7 +11,12 @@ const Boton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
-  cursor: pointer;
+  transition: background-size .8s ease;
+  
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 const Contenedor = styled.div`
@@ -20,18 +26,25 @@ const Contenedor = styled.div`
   flex-direction: column;
 `
 
-
 function App() {
   const [frase, guardarFrase] = useState({});
-  
+
   const consultarAPI = async () => {
   const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
   const frase = await api.json();
   guardarFrase(frase[0]);
   }
 
+  useEffect(() => {
+    consultarAPI();
+  }, [])
+  
+
   return (
     <Contenedor>
+      <Frase 
+        frase={frase}
+      />
       <Boton
         onClick={consultarAPI}
       >
